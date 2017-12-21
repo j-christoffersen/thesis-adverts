4719cf8b568cb5e4f9483b3a5b6f22d745fe2f51
Jackson Christoffersen <jacksontchristo@gmail.com>
2017-12-20 17:26:57 -0800

    small refactor

--- server/controllers/adverts.js
+++ server/controllers/adverts.js
@@ -10,20 +10,19 @@ const { redisUrl } = require('../../redisConfig.js');
 const redisClient = redis.createClient(redisUrl);
 
 const REDIS_EXPIRE_TIME = 20;
-const JsonHeaders = { 'Content-Type': 'application/json' };
 
 module.exports = {
   read: (req, res) => {
     redisClient.getAsync(req.query.userId)
       .then((adverts) => {
         if (adverts) {
-          res.set(JsonHeaders).send(adverts);
+          res.send(adverts);
           redisClient.expireAsync(req.query.userId, REDIS_EXPIRE_TIME);
         } else {
           model.Advert.recommended(req.query.userId)
             .then((result) => {
               const resultJson = JSON.stringify(result);
-              res.set(JsonHeaders).send(resultJson);
+              res.send(resultJson);
               redisClient.setAsync(req.query.userId, resultJson, 'EX', REDIS_EXPIRE_TIME);
             })
             .catch((err) => {
